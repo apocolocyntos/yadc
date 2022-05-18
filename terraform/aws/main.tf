948516371e46f3e096bb0b5147df5dbfd2bb7bec
Wulf Thimm <thimm.wulf@icloud.com>
2022-05-18 21:04:35 +0200

    dev

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -27,6 +27,37 @@ resource "aws_subnet" "subnet_default_c" {
 
 
 resource "aws_security_group" "security_group_default" {
-  vpc_id = aws_vpc.vpc_default.id
-  name   = "default"
+  vpc_id      = aws_vpc.vpc_default.id
+  name        = "default"
+  description = "default VPC security group"
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+  ingress = [
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+  ]
+  revoke_rules_on_delete = true
+  tags                   = var.tags
 }
