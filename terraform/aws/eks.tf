800c1c1abdea53bd44337237e893c9aec9e583c1
Wulf Thimm <thimm.wulf@icloud.com>
2022-05-18 21:32:14 +0200

    added eks

---
+++ terraform/aws/eks.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "iam_role_eks_default" {
+  name = "eks-cluster-default"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_eks_default_EKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.iam_role_eks_default.name
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_eks_default_EKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.iam_role_eks_default.name
+}
+
+resource "aws_eks_cluster" "eks_cluster_default" {
+  name     = "default"
+  role_arn = aws_iam_role.iam_role_eks_default.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_default_a.id,
+      aws_subnet.subnet_default_b.id,
+      aws_subnet.subnet_default_c.id,
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.iam_role_policy_attachment_eks_default_EKSClusterPolicy,
+    aws_iam_role_policy_attachment.iam_role_policy_attachment_eks_default_EKSVPCResourceController,
+  ]
+}
