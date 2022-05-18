f83ba39f04f2dfe631070173ee64410b8bea1357
Wulf Thimm <thimm.wulf@gmail.com>
2022-05-18 11:10:04 +0200

    Update dhcp.tf

--- terraform/aws/dhcp.tf
+++ terraform/aws/dhcp.tf
@@ -2,5 +2,6 @@
 resource "aws_vpc_dhcp_options" "dhcp_default" {
   domain_name         = "eu-central-1.compute.internal"
   domain_name_servers = ["AmazonProvidedDNS", ]
+  ntp_servers         = ["169.254.169.123"]
   tags                = var.tags
 }
