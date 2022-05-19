b6a103b55b5eb533a28a7793aa821a9bb54dc5c9
Wulf Thimm <thimm.wulf@icloud.com>
2022-05-19 09:05:26 +0200

    dev

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -1,3 +1,7 @@
+#
+# Control-Plane
+#
+
 resource "aws_iam_role" "iam_role_eks_default" {
   name = "eks-cluster-default"
 
@@ -42,5 +46,67 @@ resource "aws_eks_cluster" "eks_cluster_default" {
   depends_on = [
     aws_iam_role_policy_attachment.iam_role_policy_attachment_eks_default_EKSClusterPolicy,
     aws_iam_role_policy_attachment.iam_role_policy_attachment_eks_default_EKSVPCResourceController,
+  ]
+}
+
+
+#
+# Node-Groups
+#
+
+resource "aws_iam_role" "iam_role_eks_node_group_default" {
+  name = "default"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_node_group_default_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.iam_role_eks_node_group_default.name
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_node_group_default_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.iam_role_eks_node_group_default.name
+}
+
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_node_group_default_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.iam_role_eks_node_group_default.name
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "node-group-default"
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids = [
+    aws_subnet.subnet_default_a.id,
+    aws_subnet.subnet_default_b.id,
+    aws_subnet.subnet_default_c.id,
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.iam_role_policy_attachment_node_group_default_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.iam_role_policy_attachment_node_group_default_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.iam_role_policy_attachment_node_group_default_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
