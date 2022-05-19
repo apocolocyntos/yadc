2c6da0af4af9f1268018ce56aaf80603e8bd1c7d
Wulf Thimm <thimm.wulf@icloud.com>
2022-05-19 09:29:43 +0200

    dev

--- terraform/aws/subnets.tf
+++ terraform/aws/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "subnet_default_a" {
   vpc_id                  = aws_vpc.vpc_default.id
   availability_zone_id    = data.aws_availability_zones.available.zone_ids[0]
   map_public_ip_on_launch = true
-  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 12, 0)
+  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 4, 0)
   tags                    = var.tags
 }
 
@@ -10,7 +10,7 @@ resource "aws_subnet" "subnet_default_b" {
   vpc_id                  = aws_vpc.vpc_default.id
   availability_zone_id    = data.aws_availability_zones.available.zone_ids[1]
   map_public_ip_on_launch = true
-  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 12, 1)
+  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 4, 1)
   tags                    = var.tags
 }
 
@@ -18,6 +18,6 @@ resource "aws_subnet" "subnet_default_c" {
   vpc_id                  = aws_vpc.vpc_default.id
   availability_zone_id    = data.aws_availability_zones.available.zone_ids[2]
   map_public_ip_on_launch = true
-  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 12, 0)
+  cidr_block              = cidrsubnet(aws_vpc.vpc_default.cidr_block, 4, 2)
   tags                    = var.tags
 }
